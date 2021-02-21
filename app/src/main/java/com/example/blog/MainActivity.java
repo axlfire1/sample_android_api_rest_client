9028599564f03e6436ca7b087a8a42a72a6a8cac
David Sarmiento <edst83@gmail.com>
2021-02-21 14:07:12 -0600

    first commit

---
+++ app/src/main/java/com/example/blog/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.blog;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.widget.Button;
+import android.widget.Toast;
+import org.json.JSONObject;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button_new_post);
+        button.setOnClickListener(v -> {
+            Context context = getApplicationContext();
+            try {
+                postRequest(context);
+            } catch (Exception e) {
+                Toast.makeText(context, e.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void postRequest(Context context) throws Exception {
+        JSONObject postDataParams = new JSONObject();
+        postDataParams.put("title", "Manjeet");
+        postDataParams.put("content", "manjeet@gmail.com");
+        ApiCaller apiCaller = new ApiCaller();
+        // apiCaller.sendPost("http://192.168.1.75:3000/api/v1/posts", postDataParams, context);
+        apiCaller.sendGet("http://192.168.1.75:3000/api/v1/posts", context);
+    }
+}
