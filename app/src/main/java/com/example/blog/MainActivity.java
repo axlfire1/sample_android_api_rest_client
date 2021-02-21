d9a60092461f8b93790703dbb55314723c9cb9e4
David Sarmiento <edst83@gmail.com>
2021-02-21 15:32:41 -0600

    add layout to main view

--- app/src/main/java/com/example/blog/MainActivity.java
+++ app/src/main/java/com/example/blog/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.blog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 import org.json.JSONObject;
 import android.os.Bundle;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,24 +18,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button button = findViewById(R.id.button_new_post);
+        final EditText textViewTitle = findViewById(R.id.editTextTitle);
+        final EditText textViewContent = findViewById(R.id.editTextContent);
+
         button.setOnClickListener(v -> {
             Context context = getApplicationContext();
             try {
-                postRequest(context);
+                String titleString = textViewTitle.getText().toString();
+                String contentString = textViewContent.getText().toString();
+                postRequest(context, titleString, contentString);
             } catch (Exception e) {
                 Toast.makeText(context, e.toString(), Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    public void postRequest(Context context) throws Exception {
+    public void postRequest(Context context, String title, String content) throws Exception {
+        // replace with your host server URI
+        String URL="http://192.168.1.87:3000/api/v1/posts";
+
         JSONObject postDataParams = new JSONObject();
-        postDataParams.put("title", "Manjeet");
-        postDataParams.put("content", "manjeet@gmail.com");
+        postDataParams.put("title", title);
+        postDataParams.put("content", content);
 
         ApiCaller apiCaller = new ApiCaller();
-
-        String URL="http://192.168.1.87:3000/api/v1/posts";
         apiCaller.Submit(URL, postDataParams.toString(), context);
     }
 }
